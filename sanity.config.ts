40abcb713763145e622603cbe63ea7ebb0274c98
Taimoor <taimoor.m89@gmail.com>
2023-01-27 22:04:26 -0500

    added logo to studio nav

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,6 +4,7 @@ import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 import { myTheme } from "./theme";
 import StudioNavbar from "./components/StudioNavbar";
+import Logo from "./components/Logo";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
@@ -20,7 +21,7 @@ export default defineConfig({
   },
   studio: {
     components: {
-      // logo: Logo,
+      logo: Logo,
       navbar: StudioNavbar,
     },
   },
