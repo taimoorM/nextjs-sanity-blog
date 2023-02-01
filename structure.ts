72d20ea5f306b1fe38c5005abd2fba3fec23385e
Taimoor <taimoor.m89@gmail.com>
2023-02-01 14:23:20 -0500

    added iframe-pane and updated preview mode for cms

---
+++ structure.ts
@@ -0,0 +1,29 @@
+import { DefaultDocumentNodeResolver } from "sanity/desk";
+import Iframe from "sanity-plugin-iframe-pane";
+
+export const getDefaultDocumentNode: DefaultDocumentNodeResolver = (
+  S,
+  { schemaType }
+) => {
+  switch (schemaType) {
+    case `post`:
+      return S.document().views([
+        S.view.form(),
+        S.view
+          .component(Iframe)
+          .options({
+            url: `${
+              process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000"
+            }/api/preview`,
+            defaultSize: "desktop",
+            reload: {
+              button: true,
+            },
+            attributes: {},
+          })
+          .title("Preview"),
+      ]);
+    default:
+      return S.document().views([S.view.form()]);
+  }
+};
